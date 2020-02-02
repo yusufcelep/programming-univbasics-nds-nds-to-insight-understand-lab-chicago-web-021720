d925c387a856958bee72d0d80cb82435276a3577
Yusuf Celep <yusufcelep8@gmail.com>
2020-02-02 03:27:31 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -5,18 +5,14 @@ require 'directors_database'
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
-  # Change the code below to pretty print the nds with pp
-  nil
   pp nds
 end
 
 def print_first_directors_movie_titles
-  row_index = 0
-  movies = directors_database[0][:movies]
-  while row_index < movies.length do
-    puts movies[row_index][:title]
-    row_index += 1
-  end
+  db = directors_database
+  db = db[0][:movies].each{|i|
+    puts i[:title]
+  }
 end
 
 #Encryption key: Skip count of 13 and alternates directions with each loop
